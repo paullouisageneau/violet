e0af94d2838cc4463b3684df1217d83e7d937ba9
Paul-Louis Ageneau <paul-louis@ageneau.org>
2021-07-04 20:49:46 +0200

    Added AGPL license headers

--- src/main.c
+++ src/main.c
@@ -1,41 +1,39 @@
 /**
  * Copyright (c) 2021 Paul-Louis Ageneau
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "options.h"
 
 #include <juice/juice.h>
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
 
-void signal_handler(int sig) {
-	(void)sig;
-}
+void signal_handler(int sig) { (void)sig; }
 
 int main(int argc, char *argv[]) {
 	signal(SIGINT, signal_handler);
 
 	violet_options_t vopts;
 	violet_options_init(&vopts);
-	if(violet_options_from_arg(argc, argv, &vopts) < 0) {
+	if (violet_options_from_arg(argc, argv, &vopts) < 0) {
 		violet_options_destroy(&vopts);
 		return EXIT_FAILURE;
 	}
